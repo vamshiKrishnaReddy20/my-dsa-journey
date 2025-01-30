ecb46d541c48baba43968ec3172adceec588442a
vamshiKrishnaReddy20 <vamshi172015@outlook.com>
2025-01-30 22:49:26 +0530

    Time: 1 ms (49.26%), Space: 2.6 MB (34.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+    for (i,&num) in nums.iter().enumerate(){
+        let mut Complement = target - num;
+
+        if let Some(&index) = map.get(&Complement){
+            return vec![index as i32,i  as i32];
+        }
+        map.insert(num,i);
+        }    
+     vec![]
+    }
+}
