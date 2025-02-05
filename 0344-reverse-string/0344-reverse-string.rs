e29d9f77c20e03e5022d2b8de2be5a3146676408
vamshiKrishnaReddy20 <vamshi172015@outlook.com>
2025-02-05 23:36:48 +0530

    Time: 0 ms (100%), Space: 5.6 MB (33.53%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.rs
+++ 0344-reverse-string/0344-reverse-string.rs
@@ -1,12 +1,8 @@
 impl Solution {
-    pub fn reverse_string(s: &mut Vec<char>) {
-        let mut array = vec![];
-        for i in (0..s.len()).rev(){
-            array.push(s[i]);
-           
-        }
-        for i in (0..s.len()){
-            s[i] = array[i];
+    pub fn reverse_string(s: &mut Vec<char>) { 
+        for left_index in (0..(s.len() / 2)){
+          let mut right_index = s.len()-1-left_index;
+          s.swap(left_index,right_index);
         }
     }
 }
